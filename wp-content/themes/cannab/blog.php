a66c6076e7014826a13bf9ca9dfcb90f16d457e2
a.sivak <riziy_bes@mail.ru>
2021-12-22 22:12:36 +0300

    create blog

---
+++ wp-content/themes/cannab/blog.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Blog
+ */
+get_header();
+?>
+        <!-- Main Side -->
+<div class="container">
+
+    <?php
+
+    $first_post = get_posts([
+        'posts_per_page' => -1,
+        'offset' => 1,
+        'post_type' => 'post',
+        'orderby' => 'date',
+        'order' => 'DESC'
+    ])[0];
+
+
+    $posts_list = get_posts([
+        'posts_per_page' => 1000,
+        'offset' => 1,
+        'post_type' => 'post',
+        'orderby' => 'date',
+        'order' => 'DESC'
+    ]);
+    ?>
+  <div class="posts">
+      <?php
+      get_template_part('/template-parts/post-template', null, array(
+          'first' => true,
+          'post' => $first_post,
+      ));
+      ?>
+
+    <p class="recent-posts-caption">Recent posts</p>
+    <div class="posts__list">
+        <?php foreach ($posts_list as $item) :
+            setup_postdata( $post );
+            get_template_part('/template-parts/post-template', null, array(
+                'first' => false,
+                'post' => $item,
+            ));
+        endforeach; wp_reset_postdata(); ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/cannab/single.php
+++ wp-content/themes/cannab/single.php
@@ -10,31 +10,167 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+  <div class="container">
+    <div class="post" id="post-<?php the_ID(); ?>">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+      <div class="post__header">
+        <figure class="post-single__img">
+            <?= get_the_post_thumbnail($post->ID, 'full'); ?>
+        </figure>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+        <div class="post__title-block">
+          <h1 class="post__caption"><?= the_title(); ?></h1>
+          <div class="post__title-block_breadcrumbs">
+            <a href="<?= get_home_url(); ?>" class="post__title-block_breadcrumb">Home</a>
+            <a href="<?= esc_url( get_permalink( get_page_by_title( 'Blog' ) ) ); ?>" class="post__title-block_breadcrumb">Blog</a>
+            <p class="post__title-block_breadcrumb"><?php the_title(); ?></p>
+          </div>
+        </div>
+      </div>
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'cannab' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'cannab' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+      <div class="post-single__wrap">
+        <div class="post-single__content">
+            <?php the_content(); ?>
+        </div>
+      </div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+    </div>
 
-		endwhile; // End of the loop.
-		?>
+    <div class="post__related-posts">
+        <?php
+        $posts_list = get_posts([
+            'posts_per_page' => 3,
+            'orderby' => 'rand',
+            'order' => 'DESC'
+        ]); ?>
 
-	</main><!-- #main -->
+      <div class="posts">
+        <p class="recent-posts-caption">Related posts</p>
+        <div class="posts__list">
+            <?php foreach ($posts_list as $item) :
+                setup_postdata( $post );
+                get_template_part('/template-parts/post-template', null, array(
+                    'first' => false,
+                    'post' => $item,
+                ));
+            endforeach; wp_reset_postdata(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <style>
+    /*body #content-sticky-panel {
+      width: 185px;
+      margin: 0;
+      padding-top: 33px;
+      top: unset;
+    }
+
+    body #content-sticky-panel:before {
+      position: absolute;
+      content: 'Contents';
+      left: 0;
+      bottom: 100%;
+      top: 0;
+      text-transform: uppercase;
+      font-size: 20px;
+      line-height: 23px;
+      color: #000;
+    }
+
+    body #content-sticky-panel a {
+      font-size: 16px;
+      line-height: 24px;
+      padding-left: 0;
+      font-weight: normal;
+      padding-top: 6px;
+      padding-bottom: 6px;
+      color: #9B9B9B;
+      transition: all ease .3s;
+      border-bottom: unset;
+    }
+
+    body #content-sticky-panel a. {
+      color: #000;
+    }
+
+    body #content-sticky-panel a:hover {
+      color: #000;
+    }
+
+    body #content-sticky-panel ul {
+      border: none;
+      border-bottom: 1px solid #ddd;
+    }*/
+
+
+    /*@media(max-width: 1600px) {
+      .post-single__img {
+        margin-left: calc((100vw - 1330px) / -2);
+      }
+
+      .post__title-block,
+      .post-single__content {
+        max-width: 900px;
+      }
+    }*/
+
+    /*@media (min-width: 1280px) and (max-width: 1500px) {
+      body #content-sticky-panel {
+        position: sticky;
+        height: 0;
+      }
+
+      body #content-sticky-panel ul {
+        overflow-y: auto;
+      }
+
+      body #mobileactivate {
+        display: none;
+      }
+    }*/
+
+
+    @media(max-width: 1400px) {
+      .post-single__img {
+        /*margin-left: calc((100vw - 1200px) / -2);*/
+      }
+    }
+
+    @media(max-width: 1280px) {
+      .post-single__img {
+        /*margin-left: calc((100vw - 1080px) / -2);*/
+      }
+    }
+
+    @media(max-width: 992px) {
+
+      /*#content-sticky-panel ul, #content-sticky-panel ul li {
+        padding-left: 7px;
+      }
+
+      body #content-sticky-panel a {
+        font-size: 14px;
+      }
+
+      body #content-sticky-panel:before {
+        text-indent: 12px;
+      }
+
+      body #content-sticky-panel:after {
+        position: absolute;
+        content: '';
+        z-index: 1;
+        background-color: #fff;
+        left: 0;
+        right: 0;
+        top: -9px;
+        height: 10px;
+      }*/
+
+    }
+  </style>
 
 <?php
-get_sidebar();
 get_footer();
