35df690144a1c624c171a36a87d16de61858032e
a.sivak <riziy_bes@mail.ru>
2021-12-25 09:30:15 +0300

    next

--- wp-content/themes/cannab/app/js/main.js
+++ wp-content/themes/cannab/app/js/main.js
@@ -68,6 +68,91 @@ setTimeout(() => {
     }
 })();
 
+(function setArchiveProductsNumbers() {
+    const widget = document.querySelector('.widget.widget_wpc_filters_widget') || null;
+
+    if (!widget) return;
+
+    const foundProductsCount = document.querySelector('.wpc-posts-found-number');
+    const categoriesList = [...document.querySelector('.wpc-filter-chips-list').children];
+
+    const foundProductsCountOut = document.querySelector('.archive-wrap_found .count');
+    const categoriesListOut = document.querySelector('.archive-wrap__header_bottom');
+
+    (function setProductsCount() {
+        if (foundProductsCount) {
+            foundProductsCountOut.innerHTML = foundProductsCount.innerHTML;
+            return;
+        }
+
+        let total = 0;
+        const categories = [...document.querySelectorAll('.wpc-filter-product_cat .wpc-term-count-value')];
+
+        categories.forEach(c => {
+            const value = parseInt(c.innerHTML);
+            total += value;
+        });
+
+        foundProductsCountOut.innerHTML = total;
+    })();
+
+    (function setActiveCats() {
+        if (!categoriesList.length) return;
+        categoriesList.forEach(c => {
+            const cat = c.querySelector('span.wpc-filter-chip-name').innerHTML;
+            if (cat !== 'Reset all') {
+                const catClasses = [...c.classList];
+                const classWithId = catClasses.filter(t => t.includes('wpc-chip-product_cat-'))[0];
+                const catId = classWithId.replace('wpc-chip-product_cat-', '');
+                const item = createCat(cat, catId);
+
+                categoriesListOut.appendChild(item);
+            }
+        });
+
+
+    })();
+
+    function createCat(title, id) {
+        const p = document.createElement('p');
+        p.classList.add('archive-wrap__header_cat');
+        p.innerHTML = title;
+
+        const span = document.createElement('span');
+        span.classList.add('remove');
+        span.innerHTML = '×';
+        span.dataset.categoryRemove = id;
+        p.appendChild(span);
+
+        span.addEventListener('click', removeCat);
+
+        return p;
+    }
+    
+    function removeCat(e) {
+        const catId = this.dataset.categoryRemove;
+        const category = document.querySelector(`label[for="wpc-checkbox-taxonomy-product_cat-${catId}"]`);
+        category.click();
+    }
+})();
+
+(function mobileSidebar() {
+    const btnShow = document.getElementById('mobile-trigger-sidebar') || null;
+    const btnHide = document.getElementById('mobile-close-sidebar') || null;
+    const wrapper = document.querySelector('.archive-container') || null;
+
+    if (!wrapper || !btnHide || !btnShow) return;
+
+    btnShow.addEventListener('click', switchSidebar);
+    btnHide.addEventListener('click', switchSidebar);
+
+    function switchSidebar(e) {
+        e.preventDefault();
+
+        wrapper.classList.toggle('activeslide');
+    }
+})();
+
 function checkWidth() {
     return mobileWidth > document.documentElement.clientWidth;
 }
