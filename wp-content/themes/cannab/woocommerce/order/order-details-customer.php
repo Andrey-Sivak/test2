65f6f229e879b99ec97f32315fb9e77407192eed
a.sivak <riziy_bes@mail.ru>
2022-01-10 17:43:40 +0300

    edit customer and order details table

--- wp-content/themes/cannab/woocommerce/order/order-details-customer.php
+++ wp-content/themes/cannab/woocommerce/order/order-details-customer.php
@@ -15,55 +15,41 @@
  * @version 5.6.0
  */
 
-defined( 'ABSPATH' ) || exit;
-
-$show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
-?>
+defined('ABSPATH') || exit; ?>
     <section class="woocommerce-customer-details">
 
-	<?php if ( $show_shipping ) : ?>
+        <p class="woocommerce-customer-details__caption">Customer and order details</p>
 
-	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
-		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+        <?php if ($order->get_billing_first_name()) : ?>
+        <div class="woocommerce-customer-details__row">
+            <p>First Name</p>
+            <strong><?= esc_html($order->get_billing_first_name()); ?></strong>
+        </div>
+        <?php endif; ?>
 
-	<?php endif; ?>
+        <?php if ($order->get_billing_last_name()) : ?>
+        <div class="woocommerce-customer-details__row">
+            <p>Last Name</p>
+            <strong><?= esc_html($order->get_billing_last_name()); ?></strong>
+        </div>
+        <?php endif; ?>
 
-	<h2 class="woocommerce-column__title"><?php esc_html_e( 'Billing address', 'woocommerce' ); ?></h2>
+        <?php if ($order->get_billing_phone()) : ?>
+            <div class="woocommerce-customer-details__row">
+                <p>Phone Number</p>
+                <strong><?= esc_html($order->get_billing_phone()); ?></strong>
+            </div>
+        <?php endif; ?>
 
-	<address>
-		<?php echo wp_kses_post( $order->get_formatted_billing_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
+        <?php if ($order->get_billing_email()) : ?>
+            <div class="woocommerce-customer-details__row">
+                <p>Phone Number</p>
+                <strong><?= esc_html($order->get_billing_email()); ?></strong>
+            </div>
+        <?php endif; ?>
 
-		<?php if ( $order->get_billing_phone() ) : ?>
-			<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
-		<?php endif; ?>
-
-		<?php if ( $order->get_billing_email() ) : ?>
-			<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
-		<?php endif; ?>
-	</address>
-
-	<?php if ( $show_shipping ) : ?>
-
-		</div><!-- /.col-1 -->
-
-		<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
-			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
-			<address>
-				<?php echo wp_kses_post( $order->get_formatted_shipping_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
-
-				<?php if ( $order->get_shipping_phone() ) : ?>
-					<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_shipping_phone() ); ?></p>
-				<?php endif; ?>
-			</address>
-		</div><!-- /.col-2 -->
-
-	</section><!-- /.col2-set -->
-
-	<?php endif; ?>
-
-	<?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
-
-</section>
+        <?php do_action('woocommerce_order_details_after_customer_details', $order); ?>
+    </section>
 </div>
 
 
@@ -71,7 +57,7 @@ $show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_a
     <p class="order-received__caption"><?php echo esc_html__('Delivery Address', 'woocommerce'); ?></p>
     <ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details order-received__table">
 
-        <?php wp_kses_post( $order->get_formatted_billing_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
+        <?php wp_kses_post($order->get_formatted_billing_address(esc_html__('N/A', 'woocommerce'))); ?>
 
         <li class="woocommerce-order-overview__order order order-received__table_row">
             <p><?php esc_html_e('Country / Region', 'woocommerce'); ?></p>
